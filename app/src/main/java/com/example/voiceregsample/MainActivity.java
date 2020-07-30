93063a2b0ccc7598c5ae224978a1e8b975fd2968
libindev <libin.developer@gmail.com>
2020-07-30 18:17:43 +0530

    fix bugs

--- app/src/main/java/com/example/voiceregsample/MainActivity.java
+++ app/src/main/java/com/example/voiceregsample/MainActivity.java
@@ -5,11 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -18,12 +16,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 
 
-
 public class MainActivity extends AppCompatActivity
-implements RecognitionListener  {
+        implements RecognitionListener {
     private GridView gridview;
     private GridAdapter gridAdapter;
     private AudioManager audioManager;
@@ -34,17 +32,20 @@ implements RecognitionListener  {
     private SpeechRecognizer speech = null;
     private Intent recognizerIntent;
     private String LOG_TAG = "VoiceRecognitionActivity";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        gridview = findViewById(R.id.grid);
-        gridAdapter = new GridAdapter(this);
-        gridview.setAdapter(gridAdapter);
-        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
+    private void resetSpeechRecognizer() {
+
+        if(speech != null)
+            speech.destroy();
         speech = SpeechRecognizer.createSpeechRecognizer(this);
-        speech.setRecognitionListener(this);
+        Log.i(LOG_TAG, "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(this));
+        if(SpeechRecognizer.isRecognitionAvailable(this))
+            speech.setRecognitionListener(this);
+        else
+            finish();
+    }
+
+    private void setRecogniserIntent() {
 
         recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
@@ -52,140 +53,137 @@ implements RecognitionListener  {
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
-
-        if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_PERMISSION);
-        }
-
-
-
     }
-    private  void mute(boolean isMute){
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int flag;
-            if(isMute)
-            {   flag=AudioManager.ADJUST_MUTE;
-            }else{
-                flag=AudioManager.ADJUST_UNMUTE;
-            }
-            audioManager.adjustStreamVolume(AudioManager.STREAM_NOTIFICATION, flag, 0);
-            audioManager.adjustStreamVolume(AudioManager.STREAM_ALARM, flag, 0);
-            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, flag, 0);
-            audioManager.adjustStreamVolume(AudioManager.STREAM_RING, flag, 0);
-            audioManager.adjustStreamVolume(AudioManager.STREAM_SYSTEM, flag, 0);
-        } else {
-            audioManager.setStreamMute(AudioManager.STREAM_NOTIFICATION, isMute);
-            audioManager.setStreamMute(AudioManager.STREAM_ALARM, isMute);
-            audioManager.setStreamMute(AudioManager.STREAM_MUSIC, isMute);
-            audioManager.setStreamMute(AudioManager.STREAM_RING, isMute);
-            audioManager.setStreamMute(AudioManager.STREAM_SYSTEM, isMute);
-        }
-    }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridview = findViewById(R.id.grid);
+        gridAdapter = new GridAdapter(this);
+        gridview.setAdapter(gridAdapter);
+
+        resetSpeechRecognizer();
+
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_PERMISSION);
+            return;
+        }
+
+        setRecogniserIntent();
+        speech.startListening(recognizerIntent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull  int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_RECORD_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    speech.startListening(recognizerIntent);
-                } else {
-                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
-                }
+
+        if (requestCode == REQUEST_RECORD_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                speech.startListening(recognizerIntent);
+            } else {
+                Toast.makeText(MainActivity.this, "Permission Denied!", Toast
+                        .LENGTH_SHORT).show();
+                finish();
+            }
         }
     }
+
+
     @Override
     public void onResume() {
+        Log.i(LOG_TAG, "resume");
         super.onResume();
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
-            speech.startListening(recognizerIntent);
-        }
-
-
+        resetSpeechRecognizer();
+        speech.startListening(recognizerIntent);
     }
+
     @Override
     protected void onPause() {
-        speech.stopListening();
+        Log.i(LOG_TAG, "pause");
         super.onPause();
-
-
+        speech.stopListening();
     }
+
     @Override
     protected void onStop() {
+        Log.i(LOG_TAG, "stop");
         super.onStop();
         if (speech != null) {
-            mute(false);
             speech.destroy();
-            Log.i(LOG_TAG, "destroy");
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        mute(false);
-        speech.destroy();
-        super.onDestroy();
-    }
 
     @Override
     public void onBeginningOfSpeech() {
         Log.i(LOG_TAG, "onBeginningOfSpeech");
-        mute(true);
-
     }
+
     @Override
     public void onBufferReceived(byte[] buffer) {
         Log.i(LOG_TAG, "onBufferReceived: " + buffer);
     }
+
     @Override
     public void onEndOfSpeech() {
-        Log.i(LOG_TAG, "onEndOfSpeech");
-
-    }
-    @Override
-    public void onError(int errorCode) {
-        String errorMessage = getErrorText(errorCode);
-        Log.d(LOG_TAG, "FAILED " + errorMessage);
         speech.stopListening();
-        speech.startListening(recognizerIntent);
     }
-    @Override
-    public void onEvent(int arg0, Bundle arg1) {
-        Log.i(LOG_TAG, "onEvent");
-    }
-    @Override
-    public void onPartialResults(Bundle arg0) {
-        Log.i(LOG_TAG, "onPartialResults");
-    }
-    @Override
-    public void onReadyForSpeech(Bundle arg0) {
-        Log.i(LOG_TAG, "onReadyForSpeech");
 
-    }
     @Override
     public void onResults(Bundle results) {
         Log.i(LOG_TAG, "onResults");
         ArrayList<String> matches = results
                 .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-
         for (String s: matches){
             if(s.contains("left")){
                 changeFocus(false);
+                break;
 
             }else if(s.contains("right")){
                 changeFocus(true);
+                break;
             }
         }
-        speech.stopListening();
-        speech.startListening(recognizerIntent);
 
+
+
+        speech.startListening(recognizerIntent);
     }
+
+    @Override
+    public void onError(int errorCode) {
+        String errorMessage = getErrorText(errorCode);
+        Log.i(LOG_TAG, "FAILED " + errorMessage);
+        speech.startListening(recognizerIntent);
+    }
+
+    @Override
+    public void onEvent(int arg0, Bundle arg1) {
+        Log.i(LOG_TAG, "onEvent");
+    }
+
+    @Override
+    public void onPartialResults(Bundle arg0) {
+        Log.i(LOG_TAG, "onPartialResults");
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle arg0) {
+        Log.i(LOG_TAG, "onReadyForSpeech");
+    }
+
     @Override
     public void onRmsChanged(float rmsdB) {
-        Log.i(LOG_TAG, "onRmsChanged: " + rmsdB);
 
     }
-    public static String getErrorText(int errorCode) {
+
+    public String getErrorText(int errorCode) {
         String message;
         switch (errorCode) {
             case SpeechRecognizer.ERROR_AUDIO:
@@ -210,7 +208,7 @@ implements RecognitionListener  {
                 message = "RecognitionService busy";
                 break;
             case SpeechRecognizer.ERROR_SERVER:
-                message = "error from server";
+                message = "Error from server";
                 break;
             case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
                 message = "No speech input";
@@ -230,12 +228,13 @@ implements RecognitionListener  {
         }
         else{
             if(GridAdapter.currentPosition!=0)
-            GridAdapter.currentPosition =GridAdapter.currentPosition -  1;
+                GridAdapter.currentPosition =GridAdapter.currentPosition -  1;
             v = gridview.getChildAt( GridAdapter.currentPosition );
         }
 
 
         v.findViewById(R.id.editText).requestFocus();
     }
+
 
 }
